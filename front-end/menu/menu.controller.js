0b3b7f0a72a1d19bf95ec1d1c8e7ef229896170a
Mark Kimball <parkskier@gmail.com>
2015-07-17 10:02:40 -0400

    ditched the directive

--- front-end/menu/menu.controller.js
+++ front-end/menu/menu.controller.js
@@ -4,9 +4,9 @@
     .module('menu')
     .controller('MenuController', function($scope, _, MenuService, $routeParams){
 
-      MenuService.getMenu().success(function(items){
-        $scope.items = items;
-      });
+      // MenuService.getMenu().success(function(items){
+      //   $scope.items = items;
+      // });
 
       if($routeParams.item_id){
         MenuService.getItem($routeParams.item_id).success(function(item){
@@ -25,6 +25,43 @@
       $scope.editItem = function(item_id, updatedItem){
         MenuService.editItem(item_id, updatedItem);
       };
+
+      $scope.sortName = 'id';
+
+      $scope.selectSort = function(sortSelected){
+        $scope.sortName = sortSelected;
+        $rootScope.$broadcast('sorter:updated');
+      };
+
+      //FAKE DATA
+      var items = [
+        {
+          name: 'Fat Belly Burger',
+          description: 'A million burgers sold!',
+          price: '24.98',
+          image: 'http://lorempixel.com/500/500/food/1/'
+        },
+        {
+          name: 'Fat Belly Braut',
+          description: 'It tastes disgusting!',
+          price: '19.97',
+          image: 'http://lorempixel.com/500/500/food/2/'
+        },
+        {
+          name: 'Fake Belly Vegan Burger',
+          description: 'Square means we care!',
+          price: '33.32',
+          image: 'http://lorempixel.com/500/500/food/3/'
+        },
+        {
+          name: 'Fake Belly Began Vraut',
+          description: 'Overpriced!',
+          price: '23.18',
+          image: 'http://lorempixel.com/500/500/food/4/'
+        },
+      ];
+
+      $scope.items = items;
 
     });
 })();
